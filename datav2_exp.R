471c9e84a95aff30a591912ac75b148a65d3bb68
miller-christopher <40681246+miller-christopher@users.noreply.github.com>
2018-09-04 00:58:08 -0500

    corrections to experiment script

--- datav2_exp.R
+++ datav2_exp.R
@@ -18,37 +18,34 @@ colnames(persons) <- clean_col_names(colnames(persons))
 colnames(rtr) <- clean_col_names(colnames(rtr))
 
 
-incid %>% data.frame() %>% select(Year.of.Incident) %>% 
+incid  %>% select(Year.of.Incident) %>% 
   filter(Year.of.Incident < 2019 & Year.of.Incident > 2013) %>%
   ggplot(aes(Year.of.Incident)) + geom_bar()
 
-incid %>% data.frame() %>% select(Day1.of.the.Week) %>% 
-  ggplot(aes(Day1.of.the.Week)) + geom_bar()
-
-incid %>% data.frame() %>% select(Day1.of.the.Year) %>% 
+incid  %>% select(Day1.of.the.Year) %>% 
   ggplot(aes(Day1.of.the.Year)) + geom_bar()
 
-incid %>% data.frame() %>% select(Day1.of.the.Year,Year.of.Incident) %>% 
+incid  %>% select(Day1.of.the.Year,Year.of.Incident) %>% 
   filter(Year.of.Incident > 2013 & Year.of.Incident < 2018) %>%
   ggplot(aes(Day1.of.the.Year)) + geom_bar()
 
-incid %>% data.frame() %>% select(Day1.of.the.Year,Year.of.Incident) %>% 
+incid  %>% select(Day1.of.the.Year,Year.of.Incident) %>% 
   filter(Year.of.Incident==2014) %>%
   ggplot(aes(Day1.of.the.Year)) + geom_bar()
 
-incid %>% data.frame() %>% select(Day1.of.the.Year,Year.of.Incident) %>% 
+incid %>% select(Day1.of.the.Year,Year.of.Incident) %>% 
   filter(Year.of.Incident==2018) %>%
   ggplot(aes(Day1.of.the.Year)) + geom_bar()
 
-incid %>% data.frame() %>% select(Month1.of.Occurence,Year.of.Incident,calcMonth) %>% 
+incid  %>% select(Month1.of.Occurrence,Year.of.Incident,calcMonth) %>% 
   filter(Year.of.Incident > 2013 & Year.of.Incident < 2018)
 
-incid %>% ggplot(aes(Month1.of.Occurence)) + geom_bar() +
+incid %>% ggplot(aes(Month1.of.Occurrence)) + geom_bar() +
   theme(axis.text.x = element_text(angle = 90))
 
 
-incid %>% data.frame() %>% select(Responding.Officer..1..Badge.No,Year.of.Incident) %>% 
-  filter(Year.of.Incident > 2014 & Year.of.Incident < 2018) %>%
-  ggplot(aes(Responding.Officer..1..Badge.No)) + geom_bar()
+incid  %>% select(Responding.Officer.1.Badge.No,Year.of.Incident) %>% 
+  filter(Year.of.Incident > 2016 & Year.of.Incident < 2018) %>%
+  ggplot(aes(Responding.Officer.1.Badge.No)) + geom_bar()
 
 
