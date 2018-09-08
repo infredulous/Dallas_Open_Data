606c6135852615900506e93674b1bb0b7ad2d881
miller-christopher <40681246+miller-christopher@users.noreply.github.com>
2018-09-08 00:37:19 -0500

    mapping more data

--- datav2_exp.R
+++ datav2_exp.R
@@ -2,7 +2,8 @@ library(tidyverse)
 library(lubridate)
 library(ggplot2)
 library(gridExtra)
-library(gghighlight)
+library(ggmap)
+#library(gghighlight)
 
 #rtr <- read_rds('Datav2/RTRv2.rds')
 #calls <- read_rds('Datav2/CAllsv2.rds')
@@ -20,47 +21,57 @@ library(gghighlight)
 
 wi <- read_rds('Datav2/condensed_incid.rds')
 
-mxx <- -96.4
-mnx <- -97
-mxy <- 33.1
-mny <- 32.6
+mxx <- -96.556819
+mnx <- -96.999444
+mxy <- 33.017063
+mny <- 32.619478
 
-w2015<- filter(wi,Year.of.Incident==2015,xcoord > mnx & xcoord < mxx,ycoord > mny & ycoord < mxy)
-w2016<- filter(wi,Year.of.Incident==2016,xcoord > mnx & xcoord < mxx,ycoord > mny & ycoord < mxy)
-w2017<- filter(wi,Year.of.Incident==2017,xcoord > mnx & xcoord < mxx,ycoord > mny & ycoord < mxy)
+w2015<- select(wi,Incident.Number,Year.of.Incident,xcoord,ycoord) %>% filter(Year.of.Incident==2015,xcoord > mnx & xcoord < mxx,ycoord > mny & ycoord < mxy)
+w2015 <- unique(w2015)
+
+
+#ng <- w2015 %>% select(Incident.Number) %>% group_by(Incident.Number) %>% mutate(cn=n())
+#ng2 <- filter(ng,cn>1)
+#idx <- which(w2015$Incident.Number %in% ng2$Incident.Number)
+
+w2016<- select(wi,Incident.Number,Year.of.Incident,xcoord,ycoord) %>% filter(Year.of.Incident==2016,xcoord > mnx & xcoord < mxx,ycoord > mny & ycoord < mxy)
+w2016 <- unique(w2016)
+
+w2017<- select(wi,Incident.Number,Year.of.Incident,xcoord,ycoord) %>% filter(Year.of.Incident==2017,xcoord > mnx & xcoord < mxx,ycoord > mny & ycoord < mxy)
+w2017 <- unique(w2017)
 
 #w2015 <- mutate(w2015,ycoord=round(ycoord,6),xcoord=round(xcoord,6))
 #w2016 <- mutate(w2016,ycoord=ycoord+.6,xcoord=xcoord+.6)
 #w2017 <- mutate(w2017,ycoord=ycoord-.6,xcoord=xcoord-.6)
 
-wi %>% select(.$xcoord==32.776328,.$ycoord==-96.797031)
+#summarize(w2015,mxx=max(xcoord),mxy=max(ycoord),mnx=min(xcoord),mny=min(ycoord),medx=median(xcoord),medy=median(ycoord),meanx=mean(xcoord),meany=mean(ycoord),sdx=sd(xcoord),sdy=sd(ycoord))
+#summarize(w2016,mxx=max(xcoord),mxy=max(ycoord),mnx=min(xcoord),mny=min(ycoord),medx=median(xcoord),medy=median(ycoord),meanx=mean(xcoord),meany=mean(ycoord),sdx=sd(xcoord),sdy=sd(ycoord))
+#summarize(w2017,mxx=max(xcoord),mxy=max(ycoord),mnx=min(xcoord),mny=min(ycoord),medx=median(xcoord),medy=median(ycoord),meanx=mean(xcoord),meany=mean(ycoord),sdx=sd(xcoord),sdy=sd(ycoord))
 
-summarize(w2015,mxx=max(xcoord),mxy=max(ycoord),mnx=min(xcoord),mny=min(ycoord),medx=median(xcoord),medy=median(ycoord),meanx=mean(xcoord),meany=mean(ycoord),sdx=sd(xcoord),sdy=sd(ycoord))
-summarize(w2016,mxx=max(xcoord),mxy=max(ycoord),mnx=min(xcoord),mny=min(ycoord),medx=median(xcoord),medy=median(ycoord),meanx=mean(xcoord),meany=mean(ycoord),sdx=sd(xcoord),sdy=sd(ycoord))
-summarize(w2017,mxx=max(xcoord),mxy=max(ycoord),mnx=min(xcoord),mny=min(ycoord),medx=median(xcoord),medy=median(ycoord),meanx=mean(xcoord),meany=mean(ycoord),sdx=sd(xcoord),sdy=sd(ycoord))
-
-specialsy <- c(32.776328,32.783960,32.843002,32.738252,32.744253,32.703065,32.832777,32.646958,32.680523,32.717153)
-specialsx <- c(-96.797031,-96.783417,-96.796746,-96.888121,-96.726645,-96.716288,-96.723129,-96.908818,-96.867250,-96.963997)
-specialsn <- c('City Hall','Deep Ellum','Highland Park','Cockrell Hill','Blair Park','Trinity Greenbelt','Whiterock Lake','Duncanville','Dallas Exec Airport','Mountain Creek Lake')
-#,'Cotton Bowl'  ,-96.7595458   ,32.779484
-
-
-
+specialsy <- c(32.783960,32.843002,32.738252,32.744253,32.703065,32.832777,32.646958,32.680523,32.717153)
+specialsx <- c(-96.783417,-96.796746,-96.888121,-96.726645,-96.716288,-96.723129,-96.908818,-96.867250,-96.963997)
+specialsn <- c('Deep Ellum','Highland Park','Cockrell Hill','Blair Park','Trinity Greenbelt','Whiterock Lake','Duncanville','Dallas Exec Airport','Mountain Creek Lake')
+#,'Cotton Bowl'  ,-96.7595458   ,32.779484   'City Hall', -96.797031,32.776328,
 specials <- data.frame(name=specialsn,x=specialsx,y=specialsy)
 
-p1 <- 
-  ggplot() + geom_point(aes(x=w2015$xcoord,y=w2015$ycoord),color='red',na.rm=TRUE,shape='triangle open') +
+p1 <- ggplot() + labs(x='Longitude',y='Latitude',title='Incidents for 2015') + geom_point(aes(x=w2015$xcoord,y=w2015$ycoord),color='red',na.rm=TRUE,shape='triangle open') +
+   geom_density_2d() +
     geom_text(aes(x=specials$x,y=specials$y,label=specials$name),color='blue',na.rm=TRUE)
-p2 <- ggplot() + geom_point(aes(x=w2016$xcoord,y=w2016$ycoord),color='blue',na.rm=TRUE,shape='circle open')
+p2 <- ggplot() + labs(x='Longitude',y='Latitude',title='Incidents for 2016') + geom_point(aes(x=w2016$xcoord,y=w2016$ycoord),color='blue',na.rm=TRUE,shape='triangle open') +
+  geom_text(aes(x=specials$x,y=specials$y,label=specials$name),color='red',na.rm=TRUE)
+  
+
 p3 <- ggplot() + geom_point(aes(x=w2017$xcoord,y=w2017$ycoord),color='green',na.rm=TRUE,shape='triangle open')
 
-grid.arrange(p1,p2,nrow=1)
 
+DallasMap <- qmap("dallas",zoom=14,color="bw",legend="topright")
+DallasMap + geom_point(aes(x=xcoord,y=ycoord),data=w2015,color="red")
 
+dallas <- get_map('dallas',zoom=14)
+DallasMap <- ggmap("dallas",extent="device",legend="topright")
 
-ggplot() + geom_point(aes(x=w2015$xcoord,y=w2015$ycoord),na.rm=TRUE,color='red') + geom_quantile() +
-  geom_point(aes(x=w2016$xcoord,y=w2016$ycoord),na.rm=TRUE,position = position_dodge(.5),color='yellow') + geom_quantile() +
-  geom_point(aes(x=w2017$xcoord,y=w2017$ycoord),na.rm=TRUE,position = position_dodge(.8),color='gray',shape='circle open') + geom_quantile()
+DallasMap + stat_density2d(aes(x=xcoord,y=ycoord),size=2,bins=4,data=w2015,geom="polygon")
+
 
 
 
